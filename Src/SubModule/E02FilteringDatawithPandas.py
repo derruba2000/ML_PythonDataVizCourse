5382a8ee939596da19ba9d914f8fbd68302e00a8
Joao Ramos <joao.ramos@uk.prime-research.local>
2019-06-19 17:50:33 +0100

    Filtering with Pandas

---
+++ Src/SubModule/E02FilteringDatawithPandas.py
@@ -0,0 +1,25 @@
+    
+import pandas as pd
+
+
+    
+import pandas as pd
+
+def RunExercise():
+    print("Filtering with Pandas")
+
+
+    melbourne_file_path = r'C:\Data\Kaggle\DataSets\melb_data.csv'
+    melbourne_data = pd.read_csv(melbourne_file_path) 
+    print(melbourne_data.columns)
+    
+    melbourne_price_data = melbourne_data.Price
+    # the head command returns the top few lines of data.
+    print(melbourne_price_data.head())
+    
+    columns_of_interest = ['Landsize', 'BuildingArea']
+    two_columns_of_data = melbourne_data[columns_of_interest]
+    print(two_columns_of_data.describe())
+    
+    
+    
--- Src/main.py
+++ Src/main.py
@@ -1,6 +1,7 @@
 import sys
 ###########################################
 import SubModule.E01AccessingDatawithPandas as EX01
+import SubModule.E02FilteringDatawithPandas as EX02
 
 
 
@@ -11,10 +12,14 @@ import SubModule.E01AccessingDatawithPandas as EX01
 ################################################################
 # Main
 if (len(sys.argv)==1):
-    print("Usage: setup <Exercise Number>")
+    print("Usage: main <Exercise Number>")
 else:
     if (sys.argv[1]=="1"):
+        print("Exercise 01")
         EX01.RunExercise()
+    if (sys.argv[1]=="2"):
+        print("Exercise 02")
+        EX02.RunExercise()
 
 
 
